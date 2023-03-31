a81d51d55474cb5b8259074bf598245b6582b994
Replit user <>
2023-03-31 17:24:01 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -61,7 +61,7 @@ void* get(ArrayList * l, int i)
 {
   if(i>=l->size) return NULL;
   if(i<0) return l->data[(l->size)+i];
-  if(i>=0) return l->data[];
+  return NULL;
 }
 
 int get_size(ArrayList * l)
